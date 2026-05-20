9fe1d95ac1423f1d590d32a7b879eabec1e3d921
morganliang <lzyrapx@gmail.com>
2026-05-20 23:43:14 +0800

    feat: 1D convolution refactor

--- cuda/1D Convolution/1D Convolution.cu
+++ cuda/1D Convolution/1D Convolution.cu
@@ -1,7 +1,3 @@
-
-// https://leetgpu.com/challenges/1d-convolution
-
-#include "solve.h"
 #include <cuda_runtime.h>
 
 __global__ void convolution_1d_kernel(const float* input, const float* kernel, float* output,
@@ -11,18 +7,18 @@ __global__ void convolution_1d_kernel(const float* input, const float* kernel, f
     if (i >= output_size) return;
 
     float sum = 0.0f;
-    for (int j = 0; j < kernel_size; ++j) {
-        sum += input[i + j] * kernel[j];
-    }
+    for (int j = 0; j < kernel_size; j++) sum += input[i + j] * kernel[j];
     output[i] = sum;
 }
 
 // input, kernel, output are device pointers (i.e. pointers to memory on the GPU)
-void solve(const float* input, const float* kernel, float* output, int input_size, int kernel_size) {
+extern "C" void solve(const float* input, const float* kernel, float* output, int input_size,
+                      int kernel_size) {
     int output_size = input_size - kernel_size + 1;
     int threadsPerBlock = 256;
     int blocksPerGrid = (output_size + threadsPerBlock - 1) / threadsPerBlock;
 
-    convolution_1d_kernel<<<blocksPerGrid, threadsPerBlock>>>(input, kernel, output, input_size, kernel_size);
+    convolution_1d_kernel<<<blocksPerGrid, threadsPerBlock>>>(input, kernel, output, input_size,
+                                                              kernel_size);
     cudaDeviceSynchronize();
 }
