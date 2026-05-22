35347a7722eefeea87ff266ccda78313baa6fc09
morganliang <lzyrapx@gmail.com>
2026-05-22 13:00:43 +0800

    feat: relu refactor

--- cuda/ReLU/ReLU.cu
+++ cuda/ReLU/ReLU.cu
@@ -1,16 +1,14 @@
-
-// https://leetgpu.com/challenges/relu-activation
-
 #include <cuda_runtime.h>
 
 __global__ void relu_kernel(const float* input, float* output, int N) {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     if (i < N) {
-        output[i] = (input[i] > 0.0f) ? input[i] : 0.0f; // ReLU操作
+        output[i] = (input[i] > 0.0f) ? input[i] : 0.0f; // ReLU
     }
 }
 
-void solve(const float* input, float* output, int N) {
+// input, output are device pointers (i.e. pointers to memory on the GPU)
+extern "C" void solve(const float* input, float* output, int N) {
     int threadsPerBlock = 256;
     int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
 
